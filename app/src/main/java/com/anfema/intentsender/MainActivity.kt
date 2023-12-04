c0eab517ffbf6599005c213a515b8f60b71a6be6
Marcel Reiter <m.reiter@anfe.ma>
2023-12-04 17:19:34 +0100

    Enhancement: add a ui for entering text

--- app/src/main/java/com/anfema/intentsender/MainActivity.kt
+++ app/src/main/java/com/anfema/intentsender/MainActivity.kt
@@ -3,13 +3,22 @@ package com.anfema.intentsender
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.anfema.intentsender.ui.theme.IntentSenderTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +26,46 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             IntentSenderTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                var textFieldValue by rememberSaveable {
+                    mutableStateOf("https://www.example.com/123")
                 }
+
+                MainScreen(
+                    textFieldValue = textFieldValue,
+                    onTextFieldValueChange = { textFieldValue = it },
+                )
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+private fun MainScreen(
+    textFieldValue: String,
+    onTextFieldValueChange: (String) -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("IntentSender") }
+            )
+        }
+    ) { paddingValues ->
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IntentSenderTheme {
-        Greeting("Android")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            OutlinedTextField(
+                value = textFieldValue,
+                singleLine = true,
+                label = { Text("Intent Url") },
+                onValueChange = { onTextFieldValueChange(it) },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
     }
 }
